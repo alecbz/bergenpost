af4b797b67e4ddb82f49be1b05c07a55c0766d4e
Alec Benzer <alecbenzer@gmail.com>
2010-01-09 13:43:20 -0500

    basic user signup/login should be working

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,19 @@
 ActionController::Routing::Routes.draw do |map|
+  map.logout '/logout', :controller => 'sessions', :action => 'destroy'
+  map.login '/login', :controller => 'sessions', :action => 'new'
+  map.register '/register', :controller => 'users', :action => 'create'
+  map.signup '/signup', :controller => 'users', :action => 'new'
+  map.resources :users
+
+  map.resource :session
+
   map.resources :articles
   map.resources :news
   map.resources :authors
+  
+  map.root({:controller => "home"})
+  map.connect ':controller/:action/:id'
+  map.connect ':controller/:action/:id.:format'
 
   # The priority is based upon order of creation: first created -> highest priority.
 
@@ -40,7 +52,4 @@ ActionController::Routing::Routes.draw do |map|
   # See how all your routes lay out with "rake routes"
 
   # Install the default routes as the lowest priority.
-  map.root({:controller => "home"})
-  map.connect ':controller/:action/:id'
-  map.connect ':controller/:action/:id.:format'
 end
