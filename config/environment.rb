4e9161ca8b43adc118a87e41ccd0c1a075040b43
Mark Fayngersh <mark@markserv.(none)>
2010-01-11 11:25:25 -0500

    fuxk this shit:

--- config/environment.rb
+++ config/environment.rb
@@ -26,7 +26,8 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem "acts_as_ferret", :version => "0.4.4"
-  # Only load the plugins named here, in the order given. By default, all plugins 
+  config.gem "activecouch", :version => "0.1.0"
+# Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
   # :all can be used as a placeholder for all plugins not explicitly named
   # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
