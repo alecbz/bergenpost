e9c21b5261bdb4e393c253522fe3ac55fd8baba1
Mark Fayngersh <mark@markserv.(none)>
2010-01-11 11:18:50 -0500

    fixed this damn shit

--- config/environment.rb
+++ config/environment.rb
@@ -9,7 +9,6 @@
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
-require 'acts_as_ferret'
 
 Rails::Initializer.run do |config|
   # Settings in config/environments/* take precedence over those specified here.
@@ -26,7 +25,7 @@ Rails::Initializer.run do |config|
   # config.gem "bj"
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "aws-s3", :lib => "aws/s3"
-
+  config.gem "acts_as_ferret"
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
   # :all can be used as a placeholder for all plugins not explicitly named
