3e58a5ee178620916e391048502fadda2b8144b8
Alec Benzer <alecbenzer@gmail.com>
2010-02-09 14:36:15 -0500

    changed index location for acts_as_ferret

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,3 +20,6 @@ config.action_view.cache_template_loading            = true
 
 # Disable delivery errors, bad email addresses will be ignored
 # config.action_mailer.raise_delivery_errors = false
+
+require 'acts_as_ferret'
+ActsAsFerret.index_dir = "#{RAILS_ROOT}/tmp/index"
