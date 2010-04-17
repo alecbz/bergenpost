11aa9b0c1f173d76e3eaa1b3f09ee8d748efadae
Alec Benzer <alecbenzer@gmail.com>
2010-04-17 15:21:43 -0400

    removed sphinx gem dependncies

--- config/environment.rb
+++ config/environment.rb
@@ -15,7 +15,7 @@ Rails::Initializer.run do |config|
   config.gem "ferret"
   config.gem "s3"
   config.gem "mime-types", :lib => "mime/types"
-  config.gem "thinking-sphinx", :lib => "thinking_sphinx", :version => '1.3.16'
+#  config.gem "thinking-sphinx", :lib => "thinking_sphinx", :version => '1.3.16'
   
   config.time_zone = 'UTC'
   
